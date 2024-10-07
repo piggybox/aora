b064401640ef511729d876b8ef9915c7e79152ed
Sky Yin <sky.yin@gmail.com>
2024-10-07 04:34:12 -0700

    TS syntax

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from "react-native"
 
-const Profile = () => {
+export default function Profile() {
     return (
         <View>
             <Text>Profile</Text>
         </View>
     )
 }
-
-export default Profile
-
-const styles = StyleSheet.create({})
