51475e6fdafbcad5102bc64c3b9afb5ff564d538
Sky Yin <sky.yin@gmail.com>
2024-10-06 21:06:27 -0700

    Nice font

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Hello world!</Text>
+      <Text className="text-3xl font-pblack">Hello world!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>
         Go to Profile
